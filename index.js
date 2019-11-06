ce5577aa1798f05a358e4744443ec0e890fac18b
Anina Mu <aninamu@protonmail.com>
2019-11-06 10:44:04 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var ctx = document.getElementById('myChart').getContext('2d')
+console.log(ctx)
+var chart = new Chart(ctx, {
+  type: 'line',
+
+  data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45]
+    }]
+  },
+
+  options: {}
+})
