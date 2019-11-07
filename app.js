33c14032e597336dc607e5429a38471c4ce31b69
Anina Mu <aninamu@protonmail.com>
2019-11-07 11:49:57 -0500

    Set up endpoint for queried data

--- app.js
+++ app.js
@@ -9,34 +9,33 @@ AWS.config.update({
 })
 AWS.config.loadFromPath('./config.json')
 const docClient = new AWS.DynamoDB.DocumentClient()
+const dbParams = {
+  TableName: 'VoltzwagenTable',
+  ProjectionExpression: 'ID, #ts, Payload',
+  ExpressionAttributeNames: {
+    '#ts': 'Timestamp',
+  },
+}
 
 app.use('/public', express.static(__dirname + '/public'))
 
-queryData(docClient)
 app.get('/', function (req, res) {
-  // data = queryData(docClient)
   res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.get('/query-data', (req, res) => {
+  docClient.scan(dbParams, (err, data) => {
+    if (err) {
+      console.log(err)
+    } else {
+      items = data.Items.sort((a, b) => {
+        return a.timestamp > b.timestamp
+      })
+      res.send(data.Items)
+    }
+  })
 })
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
 })
-
-////////////////////////////////////////////////////////////////////////////////
-function queryData(docClient) {
-  let params = {
-    TableName: 'VoltzwagenTable',
-    ProjectionExpression: 'ID, #ts, Payload',
-    ExpressionAttributeNames: {
-      '#ts': 'Timestamp',
-    },
-  }
-
-  docClient.scan(params, (err, data) => {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log(data.Items)
-    }
-  })
-}
